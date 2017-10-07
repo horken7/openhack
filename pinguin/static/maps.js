a43e9211a9b424cb7ebad087443788845f36be98
kisv701 <kisv701@gmail.com>
2017-10-07 13:11:06 +0200

    Added some extra functionality to the map (heatmap, dynamic scroll markers)

--- pinguin/static/maps.js
+++ pinguin/static/maps.js
@@ -2,101 +2,98 @@
  * Created by Kim on 2017-10-06.
  */
 var MAP_API_KEY = "AIzaSyDdeEbc5m6P5em_LlQ92oxsufsCU5hNadE";
-var mapCenter = {lat: 61.004627, lng: 14.537272};
-/**
- * List of map markers where a marker has type (work/home), position (latitude longitude), and a title which is some text
- */
-var mapMarkers = [{type: 'home',pos: {lat: 61.014627, lng: 14.538272}, title: '2a 55kvm'},
-    {type: 'home',pos: {lat: 61.006627, lng: 14.537472}, title: '3a 75kvn'},
-    {type: 'work',pos: {lat: 61.004627, lng: 14.582272}, title: 'kallkökskock'},
-    {type: 'work',pos: {lat: 61.026627, lng: 14.589472}, title: 'Skidvallare'}];
+var mapCenter = {lat: 61.004627, lng: 14.537272}; //Initial focus of the map
+var mapMarkersArray = [];
+var heatMapData = [];
+var heatmapZoomTresh = 13;
+
 
 var mapIcons = {
-    work: '/static/icons/work.png',
-    home: '/static/icons/home.png'
+    work: 'icons/work.png',
+    home: 'icons/home.png'
 };
-var map;
+var map, heatmap;
 /**
  * Styling the map
  */
 var mapStyle = [
-    {elementType: 'geometry', stylers: [{color: '#cccc3e'}]},
-    {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-    {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
     {
-        featureType: 'administrative.locality',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#d59563'}]
+        featureType: "administrative",
+        elementType: "labels.text.fill",
+        stylers: [
+            {
+                color: "#444444"
+            }
+        ]
     },
     {
-        featureType: 'poi',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#d59563'}]
+        featureType: "landscape",
+        elementType: "all",
+        stylers: [
+            {
+                color: "#f2f2f2"
+            }
+        ]
     },
     {
-        featureType: 'poi.park',
-        elementType: 'geometry',
-        stylers: [{color: '#263c3f'}]
+        featureType: "poi",
+        elementType: "all",
+        stylers: [
+            {
+                visibility: "off"
+            }
+        ]
     },
     {
-        featureType: 'poi.park',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#6b9a76'}]
+        featureType: "road",
+        elementType: "all",
+        stylers: [
+            {
+                saturation: -100
+            },
+            {
+                lightness: 45
+            }
+        ]
     },
     {
-        featureType: 'road',
-        elementType: 'geometry',
-        stylers: [{color: '#38414e'}]
+        featureType: "road.highway",
+        elementType: "all",
+        stylers: [
+            {
+                visibility: "simplified"
+            }
+        ]
     },
     {
-        featureType: 'road',
-        elementType: 'geometry.stroke',
-        stylers: [{color: '#212a37'}]
+        featureType: "road.arterial",
+        elementType: "labels.icon",
+        stylers: [
+            {
+                visibility: "off"
+            }
+        ]
     },
     {
-        featureType: 'road',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#9ca5b3'}]
+        featureType: "transit",
+        elementType: "all",
+        stylers: [
+            {
+                visibility: "off"
+            }
+        ]
     },
     {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [{color: '#746855'}]
-    },
-    {
-        featureType: 'road.highway',
-        elementType: 'geometry.stroke',
-        stylers: [{color: '#1f2835'}]
-    },
-    {
-        featureType: 'road.highway',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#f3d19c'}]
-    },
-    {
-        featureType: 'transit',
-        elementType: 'geometry',
-        stylers: [{color: '#2f3948'}]
-    },
-    {
-        featureType: 'transit.station',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#d59563'}]
-    },
-    {
-        featureType: 'water',
-        elementType: 'geometry',
-        stylers: [{color: '#17263c'}]
-    },
-    {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#515c6d'}]
-    },
-    {
-        featureType: 'water',
-        elementType: 'labels.text.stroke',
-        stylers: [{color: '#17263c'}]
+        featureType: "water",
+        elementType: "all",
+        stylers: [
+            {
+                color: "#46bcec"
+            },
+            {
+                visibility: "on"
+            }
+        ]
     }
 ];
 
@@ -107,31 +104,138 @@ var mapStyle = [
  */
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 6,
+        zoom: 5,
         center: mapCenter,
         styles: mapStyle
     });
+
+    /* //Center marker, uncomment to more clearly see the initial focus point of the map
     var marker = new google.maps.Marker({
         position: mapCenter,
         map: map
     });
+    */
 
+    heatmap = new google.maps.visualization.HeatmapLayer({
+        data: heatMapData,
+        map: map
+    });
 
+    //Redraw when window changes
     google.maps.event.addDomListenerOnce(map, 'idle', function () {
         google.maps.event.addDomListener(window, 'resize', function () {
             map.setCenter(mapCenter);
         })
     });
+
+    //When zooming in adapt the heatmaps
+    map.addListener('zoom_changed', function () {
+
+        if(map.zoom < heatmapZoomTresh) {
+            if(!heatmap.getMap()) {
+                heatmap.setMap(map);
+            }
+            //Magical power 3 divided by 6 because it looked like a nice scaling on my computer
+            heatmap.set('radius', Math.pow(map.zoom, 3) / 6);
+            var newdata = new google.maps.MVCArray(heatMapData);
+            heatmap.set('data', newdata);
+
+            removeMapMarkers();
+
+        } else {
+            if(heatmap.getMap())
+                heatmap.set('map',null);
+                //TODO: Figure out a nice way to call with correct markers
+                addMapMarkers(mapTestMarkers);
+        }
+    });
+
 }
 
-function drawMapMarkers(markers){
+
+/**
+ * @param markersToRemove, optional argument array of id tags used when adding the marker
+ * If no parameter is given will clear all markers from the map
+ */
+function removeMapMarkers(markersToRemove) {
+
+    if(typeof markersToRemove === "undefined") {
+        for (var i = 0; i < mapMarkersArray.length; i++) {
+            mapMarkersArray[i].setMap(null);
+        }
+        mapMarkersArray.length = 0;
+    } else {
+        //Find and remove markers with matching IDs
+        for(var j=0; j < markersToRemove.length; j++) {
+            for (i = 0; i < mapMarkersArray.length; i++) {
+                if (markersToRemove[j] === mapMarkersArray[i].marker_id) {
+                    mapMarkersArray[i].setMap(null);
+                    mapMarkersArray.splice(i, 1);
+                    break; //Found ID
+                }
+            }
+        }
+    }
+}
+
+function addMapMarkers(markers){
     markers.forEach(function (mark) {
         var marker = new google.maps.Marker({
             position: mark.pos,
             icon: mapIcons[mark.type],
-            map: map
+            map: map,
 
+            //Custom attributes
+            marker_id: mark.id
+        });
+        marker.addListener('click',function(){
+            console.log(marker.marker_id);
+        });
+        mapMarkersArray.push(marker);
+
+    });
+}
+
+/**
+ *
+ * @param cities, array of cities
+ */
+function addMapHeatmap(cities) {
+
+    cities.forEach(function(city){
+        heatMapData.push({
+            location: new google.maps.LatLng(city.pos.lat, city.pos.lng),
+            weight: city.temperature,
+            city_id: city.id
         });
     });
 
+
+
+    var gradient = [
+        'rgba(0, 255, 255, 0)',
+        'rgba(0, 255, 255, 1)',
+        'rgba(0, 191, 255, 1)',
+        'rgba(0, 127, 255, 1)',
+        'rgba(0, 63, 255, 1)',
+        'rgba(0, 0, 255, 1)',
+        'rgba(0, 0, 223, 1)',
+        'rgba(0, 0, 191, 1)',
+        'rgba(0, 0, 159, 1)',
+        'rgba(0, 0, 127, 1)',
+        'rgba(63, 0, 91, 1)',
+        'rgba(127, 0, 63, 1)',
+        'rgba(191, 0, 31, 1)',
+        'rgba(255, 0, 0, 1)'
+    ];
+
+    var newdata = new google.maps.MVCArray(heatMapData);
+    heatmap.set('data', newdata);
+    heatmap.set('gradient', gradient);
+    heatmap.set('radius', map.zoom*4);
+    heatmap.set('opacity',0.5);
+}
+
+function toggleHeatmap(){
+    heatmap.setMap(heatmap.getMap() ? null : map);
 }
